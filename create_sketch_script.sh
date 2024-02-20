6995d090f8296b3be20c71cd527fe192c0552317
AndreaBruno97 <andbruno97@gmail.com>
2024-02-20 22:52:07 +0100

    Sketch creation script now creates a .ino file with a starting structure.

--- create_sketch_script.sh
+++ create_sketch_script.sh
@@ -8,3 +8,11 @@ cd $1
 touch "$1.ino"
 touch "$1.md"
 mkdir "media"
+
+echo "void setup() {" >> "$1.ino"
+echo -e '\t' >> "$1.ino"
+echo "}" >> "$1.ino"
+echo "" >> "$1.ino"
+echo "void loop() {" >> "$1.ino"
+echo -e '\t' >> "$1.ino"
+echo "}" >> "$1.ino"
